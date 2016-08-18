84c6b748b704247ef777adb4f5e63dbdc725bce9
mounier <dridri_du91@hotmail.fr>
2016-08-18 14:42:22 +0200

    routing done. http done.

---
+++ app/app.routing.ts
@@ -0,0 +1,20 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent }      from './dashboard.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+
+
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
